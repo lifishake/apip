95bb628c495c489229f57dfc417da30dfa1354fe
lifishake <lifishake@gmail.com>
2017-03-30 12:18:22 +0800

    只是改个版本号

--- apip.php
+++ apip.php
@@ -7,7 +7,7 @@
  * Description: Plugins used by pewae
  * Author:      lifishake
  * Author URI:  http://pewae.com
- * Version:     1.17
+ * Version:     1.2.1
  * License:     GNU General Public License 3.0+ http://www.gnu.org/licenses/gpl.html
  */
 
