f2c77039d23c1d14d14d063cc501a693859fc1fe
lifishake <lifishake@gmail.com>
2024-02-21 12:50:03 +0800

    Txt是对的.不显示天气的原因是函数升级时数据库没处理好.

--- apip.php
+++ apip.php
@@ -7,7 +7,7 @@
  * Description: Plugins used by pewae
  * Author:      lifishake
  * Author URI:  http://pewae.com
- * Version:     1.37.0
+ * Version:     1.37.1
  * License:     GNU General Public License 3.0+ http://www.gnu.org/licenses/gpl.html
  */
 
